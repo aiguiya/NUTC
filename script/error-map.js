3c68da1a2075c647feece14853db2a9e4d738179
aiguiya <32112876+aiguiya@users.noreply.github.com>
2018-04-06 16:58:00 -0500

    Update error-map.js

--- script/error-map.js
+++ script/error-map.js
@@ -1,1 +1,23 @@
+/**
+* error-map.js
+*
+* Renders the "speed error" chart and map-glyph in the following stages:
+*
+* 1. Load the required data from TrEPS Validation
+* 2. Render the horizon/color band chart
+* 3. Render the map-glyph for this section
+* 4. Render the line-width key for the map glyph
+* 5. Render the error color band key
+* 6. Render the horizon entries per minute chart key
+* 7. Set up the click and hover interaction with surrounding text
+*
+* Interaction is added to all elements throughout as they are rendered.
+*/
+
+
+
+
+/* 1. Load the required data from TrEPS Validation
+****************************************************************************/
+
 
